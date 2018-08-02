40f8552a20c24bbefcaf1ef9106da7558d579630
Diego Ferigo <diego.ferigo@iit.it>
2018-08-02 15:50:58 +0200

    Updated Travis CI to stage-based pipeline
    Updated also the pulled docker image

--- .ci/build_deps.sh
+++ .ci/build_deps.sh
@@ -2,33 +2,47 @@
 set -e
 set -u
 
-mkdir $HOME/git
+# Folder where to clone repositories
+GIT_FOLDER=$HOME/git
+mkdir -p $GIT_FOLDER
 
-# Build and install yarp
-cd $HOME/git
-git clone --depth 1 -b $DEPS_BRANCH https://github.com/robotology/yarp.git
-cd yarp
-mkdir build && cd build
-cmake .. \
-    -G"$TRAVIS_CMAKE_GENERATOR" \
-    -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE \
-    -DCMAKE_INSTALL_PREFIX=$DEPS_CACHE \
-    -DCREATE_LIB_MATH=ON
-cmake --build . --config $TRAVIS_BUILD_TYPE --target install
+if [ "${TRAVIS_OS_NAME}" = "osx" ] ; then
+     # Build and install ycm
+    cd $GIT_FOLDER
+    git clone --depth 1 -b $DEPS_BRANCH https://github.com/robotology/ycm.git
+    cd ycm
+    mkdir build && cd build
+    cmake .. \
+        -G"$TRAVIS_CMAKE_GENERATOR" \
+        -DCMAKE_INSTALL_PREFIX=$DEPS_INSTALL_PREFIX
+    cmake --build . --target install
 
-# Build and install idyntree
-cd $HOME/git
-git clone --depth 1 -b $DEPS_BRANCH https://github.com/robotology/idyntree.git
-cd idyntree
-mkdir build && cd build
-cmake .. \
-    -G"$TRAVIS_CMAKE_GENERATOR" \
-    -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE \
-    -DCMAKE_INSTALL_PREFIX=$DEPS_CACHE
-cmake --build . --config $TRAVIS_BUILD_TYPE --target install
+    # Build and install yarp
+    cd $GIT_FOLDER
+    git clone --depth 1 -b $DEPS_BRANCH https://github.com/robotology/yarp.git
+    cd yarp
+    mkdir build && cd build
+    cmake .. \
+        -G"$TRAVIS_CMAKE_GENERATOR" \
+        -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE \
+        -DCMAKE_INSTALL_PREFIX=$DEPS_INSTALL_PREFIX \
+        -DCREATE_LIB_MATH=ON
+    cmake --build . --config $TRAVIS_BUILD_TYPE --target install
+
+    # Build and install idyntree
+    cd $GIT_FOLDER
+    git clone --depth 1 -b $DEPS_BRANCH https://github.com/robotology/idyntree.git
+    cd idyntree
+    mkdir build && cd build
+    cmake .. \
+        -G"$TRAVIS_CMAKE_GENERATOR" \
+        -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE \
+        -DCMAKE_INSTALL_PREFIX=$DEPS_INSTALL_PREFIX
+    cmake --build . --config $TRAVIS_BUILD_TYPE --target install
+fi
 
 # Build and install xsense-mvn
-cd $HOME/git
+cd $GIT_FOLDER
 git clone https://github.com/robotology-playground/xsens-mvn
 cd xsens-mvn
 mkdir build && cd build
